cab91e9315fbf576fbb627500574fbb52e1f792c
neimarfelizzola <neimarfelizzola3o@gmail.com>
2022-11-22 20:13:40 -0500

    ultimoo

---
+++ VetyShop/front/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export const Footer = () => {
+  return (
+    <Fragment>
+        <footer className="py-1">
+            <p className="text-center mt-1">
+                Five Code Store - Mision TIC 
+            </p>
+        </footer>
+
+    </Fragment>
+  )
+}
